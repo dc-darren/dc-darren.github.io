f89a1e1662a6fd0b89e0beae2706be5508151194
dc-darren <darren.denisson@gmail.com>
2022-11-19 22:49:18 +0700

    Imported fonts and created navbar
    - Customized button styles
    - Imported 'Manrope' and 'Inter' from Google Fonts
    - Added sample image asset

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 function App() {
   return (
-    <div className="min-h-screen bg-dc-white flex p-4">
-      <div className="h-fit flex flex-col gap-4 bg-dc-black p-4">
-        <button className="btn-desktop btn-dc-red">Button Red</button>
-        <button className="btn-desktop btn-dc-red-outline">Button Red Outline</button>
-        <button className="btn-desktop btn-dc-white">Button White</button>
-        <button className="btn-desktop btn-dc-white-outline">Button White Outline</button>
+    <>
+      {/* Navbar */}
+      <header className="bg-dc-white">
+        <nav className="container-dc flex justify-between items-center text-dc-red px-20 py-5">
+          <a href="/" className="font-bold text-xl">dc-darren</a>
+          <div className="flex items-center gap-10">
+            <a href="#about" className="font-semibold">About</a>
+            <a href="#portfolio" className="font-semibold">Portfolio</a>
+            <a href="#getInTouch" className="btn-desktop btn-dc-red-outline">Get in Touch</a>
+          </div>
+        </nav>
+      </header>
+      {/* End of Navbar */}
+      <div className="min-h-screen bg-dc-white flex p-4">
+        <div className="h-fit flex flex-col gap-4 bg-dc-black p-4">
+          <button className="btn-desktop btn-dc-red">Button Red</button>
+          <button className="btn-desktop btn-dc-red-outline">Button Red Outline</button>
+          <button className="btn-desktop btn-dc-white">Button White</button>
+          <button className="btn-desktop btn-dc-white-outline">Button White Outline</button>
+        </div>
+        <div className="h-fit flex flex-col gap-4 bg-dc-black p-4 ml-4">
+          <button className="btn-desktop btn-dc-red">Button Desktop</button>
+          <button className="btn-tablet btn-dc-red">Button Tablet</button>
+          <button className="btn-mobile btn-dc-red">Button Mobile</button>
+        </div>
       </div>
-      <div className="h-fit flex flex-col gap-4 bg-dc-black p-4 ml-4">
-        <button className="btn-desktop btn-dc-red">Button Desktop</button>
-        <button className="btn-tablet btn-dc-red">Button Tablet</button>
-        <button className="btn-mobile btn-dc-red">Button Mobile</button>
-      </div>
-    </div>
+    </>
   );
 }
 
