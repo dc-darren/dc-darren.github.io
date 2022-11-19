3f2e109647f9a913e502868b7ae4ffcb0ce2307e
dc-darren <darren.denisson@gmail.com>
2022-11-19 22:07:49 +0700

    CSS custom buttons

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 function App() {
   return (
-    <div className="bg-blue-50 min-h-screen p-4">
-      <h1 className="font-bold text-blue-500 text-3xl">Tailwind Init</h1>
+    <div className="min-h-screen bg-dc-white flex p-4">
+      <div className="h-fit flex flex-col gap-4 bg-dc-black p-4">
+        <button className="btn-desktop btn-dc-red">Button Red</button>
+        <button className="btn-desktop btn-dc-red-outline">Button Red Outline</button>
+        <button className="btn-desktop btn-dc-white">Button White</button>
+        <button className="btn-desktop btn-dc-white-outline">Button White Outline</button>
+      </div>
+      <div className="h-fit flex flex-col gap-4 bg-dc-black p-4 ml-4">
+        <button className="btn-desktop btn-dc-red">Button Desktop</button>
+        <button className="btn-tablet btn-dc-red">Button Tablet</button>
+        <button className="btn-mobile btn-dc-red">Button Mobile</button>
+      </div>
     </div>
   );
 }
