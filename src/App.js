77a433bbd661bfaff76f22fa5a9b1d5cf6952499
dc-darren <darren.denisson@gmail.com>
2023-02-03 12:14:43 +0700

    Main portofolio assets + copywriting and button hover style

--- src/App.js
+++ src/App.js
@@ -48,41 +48,37 @@ function App() {
         {/* Portfolio */}
 
         {/* Portfolio Image Left */}
-        <section id="portfolio-1" className="bg-dc-gray">
+        <section id="portfolio-1" className="bg-[#7A6657]">
           <div className="dc-container dc-grid items-center gap-10 px-30">
             {/* Image width 50% of screen = (col 1 till col 7) + (gap/2) */}
             {/* With grid applied, image width is also added with margin which is 120px */}
-            <img src="assets/images/sample-image.png" alt="Avatar" className="col-start-1 col-end-7 min-w-[calc(100%__+_(2.5rem/2)+_120px)] -ml-30" />
+            <img src="assets/images/daddj-poster.png" alt="Daddj Poster" className="col-start-1 col-end-7 min-w-[calc(100%__+_(2.5rem/2)+_120px)] object-left -ml-30" />
             <div className="col-start-8 col-span-5 flex flex-col gap-3 text-dc-white">
-              <p className="dc-subheader text-lg">This is a header</p>
-              <h2 className="dc-header text-5xl">This is also a header</h2>
+              <p className="dc-subheader text-lg">API Fetching with React</p>
+              <h2 className="dc-header text-5xl">Daddj</h2>
               <p className="flex-shrink-0 basis-1/2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Ut maecenas ultricies eget viverra aliquam eget nec metus, id.
-                Eget sagittis fames blandit lorem mattis.
+                A simple website made with React, showing dad jokes fetched from a public API. Optimize the search feature to find a dad joke that fits your style.
               </p>
-              <a href="#portfolio-1" className="btn-desktop btn-dc-white-outline mt-2">Button</a>
+              <a href="https://dc-darren.github.io/daddj/" target="_blank" rel="noreferrer" className="btn-desktop btn-dc-white-outline hover:text-[#7A6657] mt-2">Open Daddj</a>
             </div>
           </div>
         </section>
         {/* End of Portfolio Image Left */}
 
         {/* Portfolio Image Right */}
-        <section id="portfolio-2" className="bg-dc-gray">
+        <section id="portfolio-2" className="bg-[#113448]">
           {/* Grid reversed for image right */}
           <div className="dc-container dc-grid direction-rtl justify-end items-center gap-10 px-30">
             {/* Image width 50% of screen = (col 1 till col 7) + (gap/2) */}
             {/* With grid applied, image width is also added with margin which is 120px */}
-            <img src="assets/images/sample-image.png" alt="Avatar" className="col-start-1 col-end-7 min-w-[calc(100%__+_(2.5rem/2)+_120px)] -mr-30" />
+            <img src="assets/images/travelokie-poster.png" alt="Travelokie Poster" className="col-start-1 col-end-7 min-w-[calc(100%__+_(2.5rem/2)+_120px)] object-left -mr-30" />
             <div className="col-start-8 col-span-5 flex flex-col items-end gap-3 text-left text-dc-white">
-              <p className="dc-subheader text-lg">This is a header</p>
-              <h2 className="dc-header text-5xl">This is also a header</h2>
-              <p className="flex-shrink-0 basis-1/2">
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Ut maecenas ultricies eget viverra aliquam eget nec metus, id.
-                Eget sagittis fames blandit lorem mattis.
+              <p className="dc-subheader text-lg">Hotel Booking with Laravel</p>
+              <h2 className="dc-header text-5xl">Travelokie</h2>
+              <p className="direction-ltr flex-shrink-0 basis-1/2">
+                Hotel booking system built with Laravel, which focuses on registration functionality of a hotel booking. Try out the simple way of booking a hotel in a few clicks.
               </p>
-              <a href="#portfolio-2" className="btn-desktop btn-dc-white-outline mt-2">Button</a>
+              <a href="http://sledge-hammer-fuses.000webhostapp.com/" target="_blank" rel="noreferrer" className="btn-desktop btn-dc-white-outline hover:text-[#113448] mt-2">Open Travelokie</a>
             </div>
           </div>
         </section>
